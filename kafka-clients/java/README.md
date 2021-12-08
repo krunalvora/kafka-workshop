626ed8675dc1f05e15518fb2b67b70c8baa35e22
Krunal Vora <krunalvora4@gmail.com>
2021-12-07 18:17:08 -0800

    es

---
+++ kafka-clients/java/README.md
@@ -0,0 +1,5 @@
+### Java Kafka Client built with Gradle
+
+`./gradlew clean build` - Creates the Java classes from the proto files present in `src/main/proto`.
+
+Run the ProtoProducer.java to produce the proto messages to Kafka.
---
+++ kafka-clients/java/maven/src/main/java/com/github/krunalvora/kafka_clients/producer/ProducerProto.java
@@ -0,0 +1,44 @@
+package com.github.krunalvora.kafka_clients.producer;
+
+import com.github.daniel.shuy.kafka.protobuf.serde.KafkaProtobufSerializer;
+import com.github.krunalvora.kafka_clients.producer.Event1OuterClass.Event1;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerProto {
+  public static void main(String[] args) {
+    Properties properties = new Properties();
+    properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+    properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaProtobufSerializer.class);
+
+    String topic = "topic7";
+
+    Event1 event1Messqge = Event1.newBuilder().setContent("asd").setIsWhatever(true)
+
+    Event1 event1Message = Event1.newBuilder()
+          .setContent("Hello world")
+          .setIsWhatever(true)
+          .build();
+
+    ProducerRecord<String, Event1> record = new ProducerRecord<String, Event1>(topic, null, event1Message);
+
+    Producer<String, Event1> producer = new KafkaProducer<String, Event1>(properties);
+
+    producer.send(record, new Callback() {
+      @Override
+      public void onCompletion(RecordMetadata metadata, Exception exception) {
+        if (exception == null) {
+          System.out.println(metadata);
+        } else {
+          exception.printStackTrace();
+        }
+      }
+    });
+
+    producer.flush();
+    producer.close();
+  }
+}
